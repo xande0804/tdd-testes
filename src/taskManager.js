bca7272403205601bee6dcd2f23638aef08b25ee
xande0804 <alexgomessilva2006@gmail.com>
2026-03-31 20:04:39 -0300

    green: implementa filterTasks

--- src/taskManager.js
+++ src/taskManager.js
@@ -42,3 +42,15 @@ export function toggleTask(task) {
 export function removeTask(tasks, taskId) {
   return tasks.filter((task) => task.id !== taskId);
 }
+
+export function filterTasks(tasks, status) {
+  switch (status) {
+    case 'completed':
+      return tasks.filter((task) => task.completed === true);
+    case 'pending':
+      return tasks.filter((task) => task.completed === false);
+    case 'all':
+    default:
+      return [...tasks];
+  }
+}
