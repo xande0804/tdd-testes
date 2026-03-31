c0bfeb258225ad3863736aac5927297a877f7584
xande0804 <alexgomessilva2006@gmail.com>
2026-03-31 19:58:35 -0300

    green: implementa addTask

--- src/taskManager.js
+++ src/taskManager.js
@@ -20,3 +20,14 @@ export function createTask(title) {
     completed: false,
   };
 }
+
+export function addTask(tasks, title) {
+  if (!validateTitle(title)) {
+    throw new Error(
+      'Título inválido: deve ser uma string com pelo menos 3 caracteres.'
+    );
+  }
+
+  const newTask = createTask(title);
+  return [...tasks, newTask];
+}
